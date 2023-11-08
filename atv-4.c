4a617598702ea39af603f812af84346d74f9a9c0
Késia Silva <kesiasilvab6@gmail.com>
2023-11-08 16:50:39 -0300

    Create atv-4.c

---
+++ atv-4.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(){
+	/*4) faça um programa que determine e mostre os cinco
+	primeiros múltiplos de 3 considerando números maiores que 0*/
+	
+	int m, i;
+	
+	for(i=0; i<5; i++){
+		m = i * 3;
+		printf("%d ", m);
+	
+	}
+	
+}
