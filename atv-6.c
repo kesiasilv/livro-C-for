49c0c56caa447786f884a3aca713af0b0a641890
Késia Silva <kesiasilvab6@gmail.com>
2023-11-13 18:50:55 -0300

    Create atv-6.c

---
+++ atv-6.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/*6) Faça um progama que mostre uma contagem regressiva na tela,
+iniciando em 10 e terminando em 0. mostre uma mensagem "FIM" apos a contagem */
+
+int main(){
+	
+	int i;
+	
+	for(i=10; i>=0; i--){
+		printf("\n %d", i);
+	}
+	printf("\nFIM!");
+	
+}
