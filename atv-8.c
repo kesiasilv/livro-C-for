50e746ea3182f53bddd63c6d26f5d3085e6354cd
Késia Silva <kesiasilvab6@gmail.com>
2023-11-13 23:10:46 -0300

    Update atv-8.c

--- atv-8.c
+++ atv-8.c
@@ -8,13 +8,13 @@ int main(){
   int num, i, soma = 0;
   double media;
 
-  for(i=1; i<=10; i++){
+  for(i=1; i<=10; i++){//fazendo a leitura dos numeros digitados e somando com o proximo numero até terminar o laço;
     printf("digite um numero inteiro: ");
     scanf("%i", &num);
-    soma += num;
-
+    soma += num;//guardado na variavel soma o resultado da soma 
   }
-    media = soma / 10;
+  
+    media = soma / 10;//calculando a media
     printf("Media: %.2lf", media);
 
 }
